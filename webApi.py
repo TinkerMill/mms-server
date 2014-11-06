3564cfd80acba3fb627d0a564494f2559739ce0d
Analog <analog@analogpixel.org>
2014-11-06 10:23:47 -0700

    check on card number
    The endpoints will just send the cardIDnumber thing, so updated the api
    to just accept that and search for a user and access.  Also added a
    basic view for the usageLog

--- webApi.py
+++ webApi.py
@@ -75,20 +75,36 @@ def query_db(query, args=(), one=False):
     rv = cur.fetchall()
     cur.close()
     return (rv[0] if rv else None) if one else rv
-    
+ 
+def getMemberIdFromSerial(serialNumber):
+    db = get_db()
+    sql = "select id from member where badgeSerialNumber='%s'" % (serialNumber)
+    for id in query_db(sql):
+        return str( id[0])
+    return "-1"
+
 @app.cli.command('initdb')
 def initdb_command():
     """Creates the database tables."""
     init_db()
     print('Initialized the database.')
 
-@app.route("/checkAccess/<deviceId>/<memberId>/<serialNumber>")
-def checkAccess(deviceId=None, memberId=None, serialNumber=None):
-    """Return if user has access to current device
+@app.route("/matt")
+def matt():
+    return getMemberIdFromSerial('a2f49dk3')
+    
+@app.route("/checkAccess/<deviceId>/<serialNumber>")
+def checkAccess(deviceId=None, serialNumber=None):
+    """Return if serialNumber has access to current device
 
+    Given a number off the RFID badge, lookup the user that is associated
+    with that number, and then check if that user has access to that deviceid
+    
+    # test with :
+    # http://localhost:5000/checkAccess/0/a2f49dk3   <- YAY 
+    # http://localhost:5000/checkAccess/0/a2f49dk33  <- FAIL
     Args:
        deviceId (int):  The ID of the device
-       memberId (int):  The ID of the member
        serialNumber (string) : the Serial number on the badge
        
     Returns:
@@ -98,7 +114,9 @@ def checkAccess(deviceId=None, memberId=None, serialNumber=None):
           {status: false, message: "fail reason" } -- No good.
           
     """
-    log(deviceId, memberId, "Requesting Access")
+    
+    memberId = getMemberIdFromSerial(serialNumber)
+    log(deviceId, memberId, "Requesting Access for serial:" + serialNumber)
     
     db = get_db()
     sql = "select entitlements.active from entitlements,member where member.id=entitlements.memberId and entitlements.memberId=%s and entitlements.deviceId=%s and member.badgeSerialNumber='%s'" % (memberId, deviceId, serialNumber)
@@ -114,20 +132,31 @@ def checkAccess(deviceId=None, memberId=None, serialNumber=None):
     log(deviceId, memberId, "Denined Access : No Access found")       
     return json.dumps({'status': False, 'message': "No Access found."})
 
+
+@app.route("/log/usageLog")
+def showLogusageLog():
+    """
+    Show the usage log, which shows what memberId's are trying
+    to accomplish.
+    
+    http://localhost:5000/log/usageLog
+    """
+    logData = query_db("select * from usageLog")
+    return render_template('usageLog.html', logData=logData)
+
 @app.route("/list/members")
 def listMembers():
     mlist = query_db("select * from member")
     return render_template('memberList.html', mlist=mlist)
-    
-@app.route("/create/member/" , methods=['POST'])
-def createMember():
-    pass
 
 @app.route("/processData" ,  methods=['POST'])
 def processData():
     """take a cmd structure to update a table. see the testData.html
     in the static folder to see how to send valid data to this endpoint.
 
+    This will be used to create new records and update existing records
+    base on the cmd (update/new)
+    
     Args:
        cmd  : is this new or an update
        table: the table to modify
@@ -150,6 +179,10 @@ def processData():
         print cmd
         return '{status: false, message: "ACL Fail Check" }'
     
+    # when creating functions to handle data, pass in the dataStruct, and get_db()
+    # which will give it access to the database
+    # see the response format above to see what kind of string/JSON to return to the client
+    # so it knows what happened.
     if cmd == "new" and table == "member":
         response = newMember(dataStruct, get_db() )
     
