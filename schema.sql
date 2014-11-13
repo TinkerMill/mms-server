3a6c8058752610f7a2c87b29bb4abd7f8a2b1644
Daniel P Williams <dwilliams@port8080.net>
2014-11-12 19:33:25 -0700

    Cleaned up the schema file for deployment.

--- schema.sql
+++ schema.sql
@@ -1,11 +1,13 @@
+-- Drop all of the tables to wipe the database
 drop table if exists member;
 drop table if exists membership;
 drop table if exists devices;
 drop table if exists entitlements;
 drop table if exists usageLog;
 
+-- Create a members table
 create table member (
-  id integer primary key autoincrement,
+  id integer primary key auto_increment,
   membership_id integer,
   firstName text not null,
   lastName text not null,
@@ -24,15 +26,9 @@ create table member (
   badgeSerialNumber text
 );
 
-insert into member ('id','membership_id', 'firstName','lastName','companyName','email',
-'phone','emergencyPhone','emergencyName','emergencyEmail','accountDisabled',
-'isMinor', 'startDate', 'roleId','passwordHash', 'badgeSerialNumber') VALUES
-(0,0,'root','admin', 'tinkermill','admin@tinkermill.org','000-000-0000',
-'noPhone','noName','noEmail',0, 0, CURRENT_TIMESTAMP, 0, '', 'a2f49dk3' );
-
-
+-- Create a membership status table
 create table membership (
-  id integer primary key autoincrement,
+  id integer primary key auto_increment,
   membershipType integer,
   numMembers integer,
   billingAddressLine1 text,
@@ -43,26 +39,79 @@ create table membership (
   startDate integer
 );
 
+-- Create a table to store the access devices
 create table devices (
-  id integer primary key autoincrement,
+  id integer primary key auto_increment,
   deviceName text not null,
   certificiateRequired integer
 );
 
+-- Create a table to store member entitlements ( what members are allowed to access).
 create table entitlements (
-  id integer primary key autoincrement,
+  id integer primary key auto_increment,
   deviceId integer,
   memberId integer,
   grantedDate integer,
   active boolean
 );
 
-insert into entitlements ('deviceId','memberId','grantedDate', 'active') values (0,0,CURRENT_TIMESTAMP,1);
-
+-- Create an access log
+-- NOTE: MariaDB 5.5 doesn't allow CURRENT_TIMESTAMP to be a default value.  The following was used instead:
+--   timeStamp DATETIME
 create table usageLog (
-  id integer primary key autoincrement,
+  id integer primary key auto_increment,
   timeStamp DATETIME DEFAULT CURRENT_TIMESTAMP,
   memberId integer,
   deviceId integer,
   message text
+);
+
+-- Insert an admin member into the members table
+insert into member (
+  id,
+  membership_id,
+  firstName,
+  lastName,
+  companyName,
+  email,
+  phone,
+  emergencyPhone,
+  emergencyName,
+  emergencyEmail,
+  accountDisabled,
+  isMinor,
+  startDate,
+  roleId,
+  passwordHash,
+  badgeSerialNumber
+) VALUES (
+  0,
+  0,
+  'root',
+  'admin',
+  'tinkermill',
+  'admin@tinkermill.org',
+  '000-000-0000',
+  'noPhone',
+  'noName',
+  'noEmail',
+  0,
+  0,
+  CURRENT_TIMESTAMP,
+  0,
+  '',
+  'a2f49dk3'
+);
+
+-- Insert an entitlement for the admin member
+insert into entitlements (
+  deviceId,
+  memberId,
+  grantedDate,
+  active
+) values (
+  0,
+  0,
+  CURRENT_TIMESTAMP,
+  1
 );
